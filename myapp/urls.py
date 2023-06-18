611003a81cb3077f02e15436b460615788480870
it-2001 <78538222+it-2001@users.noreply.github.com>
2023-06-18 22:53:19 +0200

    Add files via upload

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,8 @@
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('seznam/', views.seznam, name='seznam'),
+    path('seznam/<int:pk>', views.Detail_kniha.as_view(), name='kniha'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView
 
-# Create your views here.
+from .models import Kniha, Autor, Nakladatelstvi
+
+
+def index(request):
+    context = {
+        'knihy': Kniha.objects.order_by('-strany')[:3],
+        "autory": Autor.objects.order_by("jmeno"),
+    }
+    return render(request, 'index.html', context=context)
+
+
+def seznam(request):
+    context = {
+        'knihy': Kniha.objects.order_by('strany'),
+        "autory": Autor.objects.order_by("jmeno"),
+    }
+    return render(request, 'seznam.html', context=context)
+
+
+class Detail_kniha(DetailView):
+    model = Kniha
+    template_name = "detail.html"
+    context_object_name = "kniha"
